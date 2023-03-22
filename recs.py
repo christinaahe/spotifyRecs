9edddadecb44bbd1954d031fa408d042bc1bc4c3
Claudia Levi <claudia@MacBook-Air.local>
2023-03-22 11:42:05 -0400

    file manipulation

--- recs.py
+++ recs.py
@@ -1,6 +1,9 @@
-from neo4j import GraphDatabase
+#from neo4j import GraphDatabase
 import subprocess
 import random
+import os
+
+
 lines = open('spotify.csv').readlines()
 random.shuffle(lines)
 
@@ -9,26 +12,42 @@ open('sample.csv', 'w').writelines(lines[:10000])
 
 def extract_songs(songs, artists):
     files=[]
+
+    cmd = "cp spotify.csv ./copy.csv"
+    subprocess.check_output(cmd, shell=True)
+    seps = '","'.join(["$" + str(i) for i in range(6, 21)])
+    cmd = 'awk -F, "{print $3","$5","$1","$2","$4","' + seps + '"}" OFS=, spotify.csv > tmp && mv tmp spotify.csv'
+    subprocess.check_output(cmd, shell=True)
     for i in range(len(songs)):
+
         # puts song in separate csv file labeled song(i).csv
-        command = "awk -F ',' '{if ($5==" + '"' + songs[i] + '"' + " && $3==" + '"' + artists[i] + '"' + ") print}' spotify.csv > song" + str(i) + ".csv"
-        line = subprocess.check_output(command, shell=True)
-        line = l
+        command = "awk -F ',' '{if ($5==" + '"' + songs[i] + '"' + " && $3==" + '"' + artists[i] + '"' + \
+                  ") print}' copy.csv > song" + str(i) + ".csv"
+        subprocess.check_output(command, shell=True)
+        cmd = "head -n 1 song" + str(i) + ".csv"
+        line = subprocess.check_output(cmd, shell=True).decode("utf-8").strip()
+        print(line)
 
         
         # adds file name to files list
         files.append("song" + str(i) + ".csv")
 
         # command to remove song from original file
-        rm_cmd = "awk -F ',' '!($5 == " + '"' + songs[i] + '"' + ")' spotify.csv > spotify.csv"
-        rm_cmd = "sed -i '' ''"
+        #rm_cmd = "awk -F ',' '!($5 == " + '"' + songs[i] + '"' + ")' spotify.csv > spotify" + str(i) + ".csv"
+        #rm_cmd = "sed -i '/.*,.*,'" + artists[i] + "',.*,'" + songs[i] + "',.*$/d' copy.csv"
+
+        rm_cmd = "sed -i '' '/^" + artists[i] + "," + songs[i]+ "/d' data.csv"
         subprocess.check_output(rm_cmd, shell=True)
 
-    sample_cmd = 'shuf -n 10000 spotify.csv > sample.csv'
+    sample_cmd = 'shuf -n 10000 copy.csv > sample.csv'
     subprocess.check_output(sample_cmd, shell=True)
 
     cat_cmd = 'cat ' + ' '.join(files) + ' sample.csv'
+    rm_cmd = 'rm copy.csv'
     subprocess.check_output(cat_cmd, shell=True)
+    subprocess.check_output(rm_cmd, shell=True)
+
+
 
 def _count_gen(reader):
     """
